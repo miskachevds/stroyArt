d4b5e6cb22810d5f6533f24262e2ea5cd9e15852
Miskachev Dima <miskachevds@yandex.ru>
2026-01-13 16:41:16 +0300

    second?

--- src/App.js
+++ src/App.js
@@ -3,8 +3,32 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      
+      <div className="header">
+        <h1>Строй АРТ</h1>
+        <div className="navigate">
+          <h2>О нас</h2>
+          <h2>Записаться</h2>
+          <h2>Контакты</h2>
+        </div>
+      </div>
+
+      <div className="main">
+        <h1>Заборы</h1>
+        <p>Предлагаем вашему вниманию наш ассортимент заборов различного класса</p>
+        <img src="" alt="1" />
+        <img src="" alt="2" />
+        <img src="" alt="3" />
+        <img src="" alt="4" />
+        <img src="" alt="5" />
+        <img src="" alt="6" />
+      </div>
+
+      <div className="footer">
+        <p>+1 123 456 78 90</p>
+        <p>hello@yandex.ru</p>
+      </div>
+
+
     </div>
   );
 }
